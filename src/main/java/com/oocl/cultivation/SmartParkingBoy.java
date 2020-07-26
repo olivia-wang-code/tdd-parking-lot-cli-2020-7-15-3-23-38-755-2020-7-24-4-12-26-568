49db22938d39b482cafdae04e7ecb18934c97271
olivia <wangol@oocl.com>
2020-07-26 18:48:59 +0800

    [AC1 of Stroy4 ][oivia]feature: pass should_return_second_parking_lot_when_parking_4th_car_given_there_is_2_cars_in_first_parking_lot_and_1_car_in_second_parking_lot()

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -5,5 +5,20 @@ import com.oocl.cultivation.exception.NotEnoughSeatException;
 import java.util.List;
 
 public class SmartParkingBoy extends ParkingBoy{
+    @Override
+    public Ticket parkCar(List<ParkingLot> parkingLots, Car car) throws NotEnoughSeatException {
+        Ticket ticket = new Ticket(car);
+        getMostEmptySeatsParkingLot(parkingLots).getParkingCarMap().put(new Ticket(car), car);
+        return ticket;
+    }
 
+    private ParkingLot getMostEmptySeatsParkingLot(List<ParkingLot> parkingLots) {
+        ParkingLot mostEmptySeatsParkingLot=parkingLots.get(0);
+        for(int i =1 ;i<parkingLots.size(); i++) {
+            if(mostEmptySeatsParkingLot.getParkingCarMap().size()>parkingLots.get(i).getParkingCarMap().size()){
+                mostEmptySeatsParkingLot=parkingLots.get(i);
+            }
+        }
+        return mostEmptySeatsParkingLot;
+    }
 }
